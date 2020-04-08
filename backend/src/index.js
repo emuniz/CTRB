171ba326cdb6e2132b0a4f781b5400f3d194f833
Eduardo Muniz <muniz.1998@hotmail.com>
2020-04-08 16:15:54 -0300

    back-end da aplicação

---
+++ backend/src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+
+//Criando a aplicação, colocando-a para ouvir a porta 3333 e respondendo a msg em JSON
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+
+app.listen(3333);
