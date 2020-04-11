b1dce509d8d863462ebb66638d3601cb901f003c
Eduardo Muniz <muniz.1998@hotmail.com>
2020-04-10 21:43:26 -0300

    front-end do app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,22 @@
+// import React, {useState} from 'react';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import './global.css';
+
+import Routes from './routes';
 
 function App() {
+  // //Para mudar o estado da informação do contador utiliza-se useState(0)
+  //  // O useState retorna um array com duas posições. [valor, funcaodeAtualizacaodovalor]
+  // const[counter, setCounter] = useState(0);
+  // //Para mudar o contador, utiliza-se setCounter
+  // function increment() {
+  //   setCounter(counter + 1) ;
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello Omnistack
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div> <Header>Contador: {counter} </Header><button onClick={increment}>Incrementar</button></div>
+    <Routes />
   );
 }
 
